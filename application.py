f4b97b981f697863134ee08002a49e3509e8bbce
Vishal Shelar <124070302+vishalshelar328@users.noreply.github.com>
2023-06-19 22:47:06 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,16 +1,30 @@
 import pickle
 import streamlit as st
 import numpy as np
-import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 # Import the Ridge regressor model and standard scaler pickle
-ridge_model = pickle.load(open('Models/ridge1.pkl', 'rb'))
-standard_scaler = pickle.load(open('Models/scaler.pkl', 'rb'))
+ridge_model = pickle.load(open('models/ridge.pkl', 'rb'))
+standard_scaler = pickle.load(open('models/scaler.pkl', 'rb'))
 
-# Set up the Streamlit app
+def validate_input(value):
+    if value not in ['0', '1']:
+        raise ValueError('Values should be either 0 or 1.')
+
 def main():
-    st.title('Welcome to Homepage')
+    st.markdown(
+        """
+        <style>
+        body {
+            background: url('path_to_your_image.jpg') no-repeat center center fixed;
+            background-size: cover;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+    st.title('Fire Weather Index(FWI) Prediction')
 
     # Display input fields
     temperature = st.text_input('Temperature', value='')
@@ -20,12 +34,24 @@ def main():
     ffmc = st.text_input('FFMC', value='')
     dmc = st.text_input('DMC', value='')
     isi = st.text_input('ISI', value='')
-    classes = st.text_input('Classes', value='')
+
     region = st.text_input('Region', value='')
+    try:
+        validate_input(region)
+    except ValueError as e:
+        st.error(str(e))
+        return  # Prevent prediction
+
+    classes = st.text_input('Classes', value='')
+    try:
+        validate_input(classes)
+    except ValueError as e:
+        st.error(str(e))
+        return  # Prevent prediction
 
     if st.button('Predict'):
-        new_data_scaled = standard_scaler.transform(
-            np.array([[temperature, rh, ws, rain, ffmc, dmc, isi, classes, region]]))
+        new_data = np.array([[temperature, rh, ws, rain, ffmc, dmc, isi, classes, region]], dtype=float)
+        new_data_scaled = standard_scaler.transform(new_data)
         result = ridge_model.predict(new_data_scaled)
 
         # Display the prediction result
